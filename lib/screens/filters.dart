53326fd0b223fbf9144691301d7bba583ff3d6ff
valentin faucon <valentin.faucon@gmail.com>
2024-10-02 16:19:11 +0200

    course section ended

---
+++ lib/screens/filters.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+enum Filter {
+  glutenFree,
+}
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({
+    super.key,
+    required this.currentFilter,
+  });
+  final Map<Filter, bool> currentFilter;
+
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreen();
+  }
+}
+
+class _FiltersScreen extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _glutenFreeFilterSet = widget.currentFilter[Filter.glutenFree]!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your filters'),
+      ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (
+          bool didPop,
+          dynamic result,
+        ) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+          });
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+              title: Text(
+                'Gluter-free',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              subtitle: Text(
+                'Only include gluten-free meals.',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
